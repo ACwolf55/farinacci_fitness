2fe00d12cb75d430d605d9e2cc671da2a2d376fa
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-09-06 18:06:27 -0400

    removed backgroundImage component

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
-const BackgroundImage = ({ imageUrl, children }) => {
-  const style = {
-    backgroundImage: `url(${imageUrl})`,
-  };
+// const BackgroundImage = ({ imageUrl, children }) => {
+//   const style = {
+//     backgroundImage: `url(${imageUrl})`,
+//   };
 
-  return (
-    <div className="bg-cover bg-center h-screen" style={style}>
-      {children}
-    </div>
-  );
-};
+//   return (
+//     <div className="bg-cover bg-center h-screen" style={style}>
+//       {children}
+//     </div>
+//   );
+// };
 
-export default BackgroundImage;
+// export default BackgroundImage;
