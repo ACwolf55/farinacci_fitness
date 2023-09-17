80c8111e9cbc1d1625402bf2a769f81e8fea6b9a
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-09-17 19:15:59 -0400

    contact email back, more temp pics/<p>s, nav stays highlighted on page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from 'next/router';
 
 
 function Header() {
+
+  const router = useRouter();
+
   return (
     <header className="px-4 md:px-24 py-4 md:flex md:items-center md:justify-between">
       <div className="flex items-center">
@@ -19,27 +23,27 @@ function Header() {
         <h2 className="text-2xl md:text-3xl ml-2">Fitness</h2>
         </div>
       </div>
-
-      <nav className="mt-4 md:mt-0 text-lg">
-        <Link href="/" className="block mt-2 md:inline-block md:mt-0 md:ml-4 hover:text-blue-500">
-          Home
-        </Link>
-        <Link href="/about" className="block mt-2 md:inline-block md:mt-0 md:ml-4 hover:text-blue-500">
-          About
-        </Link>
-        <Link href="/programs" className="block mt-2 md:inline-block md:mt-0 md:ml-4 hover:text-blue-500">
-          Programs
-        </Link>
-        <Link href="/successStories" className="block mt-2 md:inline-block md:mt-0 md:ml-4 hover:text-blue-500">
-          Success Stories
-        </Link>
-        <Link href="/contact" className="block mt-2 md:inline-block md:mt-0 md:ml-4 hover:text-blue-500">
-          Contact
-        </Link>
-     
-      </nav>
+  
+    <nav className="mt-4 md:mt-0 text-lg">
+      <Link href="/" className={`block mt-2 md:inline-block md:mt-0 md:ml-4 ${router.pathname === '/' ? 'text-blue-500' : 'hover:text-blue-500'}`}>
+        Home
+      </Link>
+      <Link href="/about" className={`block mt-2 md:inline-block md:mt-0 md:ml-4 ${router.pathname === '/about' ? 'text-blue-500' : 'hover:text-blue-500'}`}>
+        About
+      </Link>
+      <Link href="/programs" className={`block mt-2 md:inline-block md:mt-0 md:ml-4 ${router.pathname === '/programs' ? 'text-blue-500' : 'hover:text-blue-500'}`}>
+        Programs
+      </Link>
+      <Link href="/successStories" className={`block mt-2 md:inline-block md:mt-0 md:ml-4 ${router.pathname === '/successStories' ? 'text-blue-500' : 'hover:text-blue-500'}`}>
+        Success Stories
+      </Link>
+      <Link href="/contact" className={`block mt-2 md:inline-block md:mt-0 md:ml-4 ${router.pathname === '/contact' ? 'text-blue-500' : 'hover:text-blue-500'}`}>
+        Contact
+      </Link>
+    </nav>
     </header>
-  )
+  );
 }
+
 
 export default Header
--- src/pages/programs.tsx
+++ src/pages/programs.tsx
@@ -5,40 +5,51 @@ function programs() {
   return (
     <div>
       <Header/>
-      <h2>Programs</h2>
+      <h2></h2>
       <div className="flex flex-col space-y-4 md:space-x-4 md:space-y-0 md:flex-row p-4 bg-gradient-to-r from-red-700 to-red-900">
   {/* Section 1 */}
   <div className="w-full md:w-1/3 bg-white rounded-lg shadow-md p-4 hover:bg-gray-100 transition duration-300">
-    <img src="your-image-url-1.jpg" alt="Image 1" className="w-full h-48 object-cover rounded-lg" />
-    <p className="mt-4 text-gray-700 text-center md:text-left">Online Training: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <div className="mt-4 text-center md:text-left">
-    <div className="mt-4 text-center">
-      <button className="py-1 px-4 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Learn More</button>
-    </div>
+    <img src="/tyson_progress.png" alt="Image 1" className="w-full h-48 object-cover rounded-lg" />
+    <div className="text-center md:text-left">
+      <h3 className="text-xl font-semibold">Online Personal Training</h3>
+      <p className="mt-4 text-gray-700">
+        Guided personal program with follow-up for every workout
+      </p>
+      <div className="mt-4 text-center md:text-left">
+        <button className="py-1 px-4 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Learn More</button>
+      </div>
     </div>
   </div>
 
   {/* Section 2 */}
   <div className="w-full md:w-1/3 bg-white rounded-lg shadow-md p-4 hover:bg-gray-100 transition duration-300">
-    <img src="your-image-url-1.jpg" alt="Image 1" className="w-full h-48 object-cover rounded-lg" />
-    <p className="mt-4 text-gray-700 text-center md:text-left">1 on 1 Training: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <div className="mt-4 text-center md:text-left">
-    <div className="mt-4 text-center">
-      <button className="py-1 px-4 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Learn More</button>
-    </div>
+    <img src="/tires.png" alt="Image 2" className="w-full h-48 object-cover rounded-lg" />
+    <div className="text-center md:text-left">
+      <h3 className="text-xl font-semibold">Strength Building Program</h3>
+      <p className="mt-4 text-gray-700">
+        Focusing on Olympic lifts and forms to build power and athletic performance
+      </p>
+      <div className="mt-4 text-center md:text-left">
+        <button className="py-1 px-4 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Learn More</button>
+      </div>
     </div>
   </div>
 
+  {/* Section 3 */}
   <div className="w-full md:w-1/3 bg-white rounded-lg shadow-md p-4 hover:bg-gray-100 transition duration-300">
-    <img src="your-image-url-1.jpg" alt="Image 1" className="w-full h-48 object-cover rounded-lg" />
-    <p className="mt-4 text-gray-700 text-center md:text-left">Group Training: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <div className="mt-4 text-center md:text-left">
-    <div className="mt-4 text-center">
-      <button className="py-1 px-4 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Learn More</button>
-    </div>
+    <img src="/frank_squat_spot.png" alt="Image 3" className="w-full h-48 object-cover rounded-lg" />
+    <div className="text-center md:text-left">
+      <h3 className="text-xl font-semibold">Power Build Program</h3>
+      <p className="mt-4 text-gray-700">
+        Get JACK3D leggo
+      </p>
+      <div className="mt-4 text-center md:text-left">
+        <button className="py-1 px-4 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300">Learn More</button>
+      </div>
     </div>
   </div>
 </div>
+
 
     </div>
   )
