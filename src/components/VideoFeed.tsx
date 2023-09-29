daa48efcbd6e08a57af413453cf1f511284e4288
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2023-09-28 22:32:04 -0400

    borders for videos and hover red

--- src/components/VideoFeed.tsx
+++ src/components/VideoFeed.tsx
@@ -7,7 +7,7 @@ interface FacebookVideoProps {
 
 const VideoFeed: React.FC<FacebookVideoProps> = ({ embedCode,description }) => {
   return (
-<div className="w-45 md:w-1/2 lg:w-1/4 mx-auto p-4 rounded-lg group hover:bg-red-600 border-red-900 border-2 border-transparent mr-4 transition duration-300 ease-in-out mb-5 md:mb-0">
+<div className="w-45 md:w-1/2 lg:w-1/4 mx-auto p-4 rounded-lg group hover:bg-red-600 border-red-800 border-2  mr-4 transition duration-300 ease-in-out mb-5 md:mb-0">
   <p className="text-sm">{description}</p>
   <br/>
   <div className="aspect-w-16 aspect-h-9">
